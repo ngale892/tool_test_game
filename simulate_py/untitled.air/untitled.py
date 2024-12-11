235df995c641a91dc47675b18dc9621d9e5503f4
ngale44 <lenga4579@gmail.com>
2024-12-11 14:44:05 +0700

    update

--- simulate_py/untitled.air/untitled.py
+++ simulate_py/untitled.air/untitled.py
@@ -1,5 +1,5 @@
 # -*- encoding=utf8 -*-
-__author__ = "dotua"
+__author__ = "Monkey"
 
 from airtest.core.api import *
 
@@ -8,14 +8,5 @@ auto_setup(__file__)
 
 from poco.drivers.unity3d import UnityPoco
 poco = UnityPoco()
-
-
-
-
-
-
-
-
-
-
-
+p = poco("ShadowOwl(Clone)_0").get_position()[0]
+print(p)
